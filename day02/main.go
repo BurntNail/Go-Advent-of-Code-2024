e610410c443e4d6b8464abb85a3dbdba9dd45a20
Jack Maguire <jack@maguire.tech>
2024-12-03 08:35:05 +0700

    d2p2

--- day02/main.go
+++ day02/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -40,34 +41,64 @@ func main() {
 	}
 
 	safeReports := partOne(reports)
-	log.Printf("%v\n", safeReports)
+	dampenerReports := partTwo(reports)
+	fmt.Printf("%v\n%v\n", safeReports, dampenerReports)
 }
 
 func partOne(reports [][]int) int {
 	numberSafe := 0
 
 	for _, report := range reports {
-		difference := report[1] - report[0]
-		works := true
-
-		for i := 0; i < (len(report) - 1); i++ {
-			thisDifference := report[i+1] - report[i]
-
-			goingSameDirection := (difference > 0) == (thisDifference > 0)
-			isInRange := (thisDifference >= 1 && thisDifference <= 3) || (thisDifference >= -3 && thisDifference <= -1)
-
-			thisRunWorked := goingSameDirection && isInRange
-
-			if !thisRunWorked {
-				works = false
-				break
-			}
-		}
-
-		if works {
+		if reportIsSafe(report) {
 			numberSafe += 1
 		}
 	}
 
 	return numberSafe
+}
+
+func partTwo(reports [][]int) int {
+	numberSafe := 0
+
+	for _, report := range reports {
+		works := 0
+
+		for i := 0; i < len(report); i++ {
+			smaller := swapRemoveClone(report, i)
+			if reportIsSafe(smaller) {
+				works += 1
+			}
+		}
+
+		if works > 0 {
+			numberSafe += 1
+		}
+	}
+
+	return numberSafe
+}
+
+func swapRemoveClone(s []int, i int) []int {
+	newSlice := make([]int, len(s))
+	copy(newSlice, s)
+	return append(newSlice[:i], newSlice[i+1:]...)
+}
+
+func reportIsSafe(report []int) bool {
+	difference := report[1] - report[0]
+
+	for i := 0; i < (len(report) - 1); i++ {
+		thisDifference := report[i+1] - report[i]
+
+		goingSameDirection := (difference > 0) == (thisDifference > 0)
+		isInRange := (thisDifference >= 1 && thisDifference <= 3) || (thisDifference >= -3 && thisDifference <= -1)
+
+		thisRunWorked := goingSameDirection && isInRange
+
+		if !thisRunWorked {
+			return false
+		}
+	}
+
+	return true
 }
